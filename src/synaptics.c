b432bb0ac4f019e464cd73e3d52afad7de1f17a8
Sencer Selcuk <sencerselcuk@gmail.com>
2016-01-09 00:29:24 -0500

    next commit: Fixed double finger tap

--- src/synaptics.c
+++ src/synaptics.c
@@ -2025,6 +2025,7 @@ SetTapState(SynapticsPrivate * priv, enum TapState tap_state, CARD32 millis)
     case TS_START:
         priv->tap_button_state = TBS_BUTTON_UP;
         priv->tap_max_fingers = 0;
+        priv->has_seen_two_finger_scroll = FALSE;
         break;
     case TS_1:
         priv->tap_button_state = TBS_BUTTON_UP;
@@ -2198,7 +2199,6 @@ restart:
           /* Disable taps outside of the active area */
           if (!inside_active_area||(priv->has_seen_two_finger_scroll == TRUE)) {
             priv->tap_button = 0;
-            priv->has_seen_two_finger_scroll = FALSE;
           }
           SetTapState(priv, TS_2A, now);
         }
